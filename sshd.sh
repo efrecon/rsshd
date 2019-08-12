9866c461ff9ffedb0f6215685216d8a33ebead9c
bulzipke <bulzipke@naver.com>
2019-08-12 21:46:00 +0900

    Update sshd.sh

--- sshd.sh
+++ sshd.sh
@@ -69,7 +69,7 @@ fi
 
 # Allow external hosts to connect
 if [ -z "$LOCAL" -o "$LOCAL" == 0 ]; then
-    sed -i "s;\#GatewayPorts no;GatewayPorts yes;g" $SDIR/sshd_config
+    sed -i "s;\GatewayPorts no;GatewayPorts yes;g" $SDIR/sshd_config
 fi
 
 # Allow root login if a password was set.
